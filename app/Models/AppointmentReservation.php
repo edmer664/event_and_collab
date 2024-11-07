64169a94409471802c57d2251b4d61b2ce3ff79c
John Edmerson Pizarra <edmerpizarra@gmail.com>
2024-11-08 05:38:04 +0800

    Add event registration upon appointment payment and define relationships in AppointmentReservation model

--- app/Models/AppointmentReservation.php
+++ app/Models/AppointmentReservation.php
@@ -25,6 +25,27 @@ class AppointmentReservation extends Model
         $this->payment_status = 'paid';
         $this->save();
 
+        EventRegistration::create([
+            'event_id' => $this->appointmentDate->event->id,
+            'user_id' => $this->user->id,
+            'status' => 'registered',
+        ]);
         
+    }
+
+    public function appointmentDate()
+    {
+        return $this->belongsTo(AppointmentDate::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // get event_id from appointment_date_id
+    public function event()
+    {
+        return $this->belongsToThrough(Event::class, AppointmentDate::class);
     }
 }
