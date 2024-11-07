2e726c3518b2c64547d8db55ad92a35e3e6cf99d
John Edmerson Pizarra <edmerpizarra@gmail.com>
2024-11-08 05:38:16 +0800

    Add fillable properties and relationships to EventRegistration model

--- app/Models/EventRegistration.php
+++ app/Models/EventRegistration.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class EventRegistration extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'event_id',
+        'user_id',
+        'status',
+    ];
+
+    public function markAsAttended()
+    {
+        $this->status = 'attended';
+        $this->save();
+    }
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
